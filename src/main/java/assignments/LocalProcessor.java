a51f12036b4d7adbd250703d298cb0dc6e8bc39f
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-12-01 01:40:55 +0400

    solved

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -15,10 +15,10 @@ import lombok.Setter;
 @Setter
 public class LocalProcessor {
     private String processorName;
-    private Long period = 10000000000000L;
+    private Long period = 10_000_000_000_000L;
     protected String ProcessorVersion;
-    private Integer valueofCheap;
-    Scanner informationscanner;
+    private Integer valueOfCheap;
+    Scanner informationScanner;
     static LinkedList<String> stringArrayList = new LinkedList<>();
 
     public LocalProcessor(String processorName, Long period, String processorVersion, Integer valueOfCheap,
@@ -26,8 +26,8 @@ public class LocalProcessor {
         this.processorName = processorName;
         this.period = period;
         ProcessorVersion = processorVersion;
-        this.valueofCheap = valueOfCheap;
-        this.informationscanner = informationscanner;
+        this.valueOfCheap = valueOfCheap;
+        this.informationScanner = informationscanner;
         this.stringArrayList = stringArrayList;
     }
 
@@ -35,7 +35,7 @@ public class LocalProcessor {
     }
 
     @ListIteratorAnnotation
-    public void listiterator(LinkedList<String> stringList) {
+    public void listIterator(LinkedList<String> stringList) {
         stringArrayList = new LinkedList<>(stringList);
         for (int i = 0; i < period; i++) {
             System.out.println(stringArrayList.get(i).hashCode());
@@ -43,7 +43,7 @@ public class LocalProcessor {
     }
 
     @FullNameProcessorGeneratorAnnotation
-    public String fullnameProcessorgenerator(LinkedList<String> stringList) {
+    public String fullNameProcessorGenerator(LinkedList<String> stringList) {
         for (int i = 0; i < stringArrayList.size(); i++) {
             processorName+=stringList.get(i)+' ';
         }
@@ -51,10 +51,10 @@ public class LocalProcessor {
     }
 
     @ReadFullProcessorNameAnnotation
-    public void readfullprocessorname(File file) throws FileNotFoundException {
-            informationscanner = new Scanner(file);
-            while (informationscanner.hasNext()) {
-                ProcessorVersion+= informationscanner.nextLine();
+    public void readFullProcessorName(File file) throws FileNotFoundException {
+            informationScanner = new Scanner(file);
+            while (informationScanner.hasNext()) {
+                ProcessorVersion+= informationScanner.nextLine();
             }
 
     }
